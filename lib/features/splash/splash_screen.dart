38c88bd06654be85d593c17cf3944070f5a463ed
saeid721 <saeid721km@gmail.com>
2025-01-06 18:31:58 +0600

    update

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -5,7 +5,11 @@ import 'package:al_kaderia/global/constants/colors_resources.dart';
 import '../../../global/constants/images.dart';
 import '../../../global/widget/global_image_loader.dart';
 import '../../../global/widget/global_sizedbox.dart';
+import '../../domain/local/preferences/local_storage.dart';
+import '../../domain/local/preferences/local_storage_keys.dart';
+import '../../getit_locator.dart';
 import '../auth/views/signin_screen.dart';
+import '../dashboard/dashboard_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -25,10 +29,16 @@ class _SplashScreenState extends State<SplashScreen> {
 
   void route(){
     Timer(const Duration(seconds: 3), () {
-      Get.offAll(()=> const SignInScreen());
+      final String? token = locator<LocalStorage>().getString(key: StorageKeys.accessToken);
+
+      if(token == null){
+        Get.offAll(()=> const SignInScreen());
+      } else{
+        Get.offAll(()=> const DashboardScreen());
+      }
     });
   }
-  
+
   @override
   Widget build(BuildContext context) {
 
--- lib/getit_locator.dart
+++ lib/getit_locator.dart
@@ -1,23 +1,26 @@
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 import 'package:get_it/get_it.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'domain/local/preferences/local_storage.dart';
 import 'domain/local/preferences/storage_controller.dart';
 import 'domain/server/http_client/request_handler.dart';
 import 'features/auth/controller/auth_controller.dart';
-import 'preferences/shared_prefs.dart';
 
 final locator = GetIt.instance;
 
-Future init(SharedPreferences prefs) async {
-  Dio dio = Dio();
+Future<void> init(LocalStorage localStorage) async {
+  final prefs = localStorage.sharedPreference;
 
-  locator.registerLazySingleton<MyPrefs>(() => MyPrefs());
-  locator.registerLazySingleton<RequestHandler>(() => RequestHandler(dio: dio));
+  // ==# Register dependencies with GetIt for core services
+  locator.registerLazySingleton<LocalStorage>(() => localStorage);
+  locator.registerLazySingleton<RequestHandler>(() => RequestHandler(dio: Dio()));
+  locator.registerLazySingleton<StorageController>(() => StorageController(prefs: prefs));
 
-  Get.lazyPut<RequestHandler>(() => RequestHandler(dio: dio));
+  // ==# Register GetIt services within GetX for further access
+  Get.lazyPut(() => AuthController(), fenix: true);
 
-  Get.put(RequestHandler( dio: dio));
-  Get.to(StorageController());
-  Get.put(AuthController());
+  Get.lazyPut(() => locator<RequestHandler>(), fenix: true);
+  Get.lazyPut(() => locator<StorageController>(), fenix: true);
+
 }
+
